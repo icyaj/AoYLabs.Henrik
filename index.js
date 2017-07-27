a35fc908863fd60a1e545c012b616d967029a368
L33tdawg <dhillon@hitb.org>
2017-07-27 13:42:45 +0800

    Slight edits

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot')
+	res.send('Hello world, I am Henrik - An AI powered chatbot using Wit.AI for Natural Language Processing') 
 })
 
 // for Facebook verification
