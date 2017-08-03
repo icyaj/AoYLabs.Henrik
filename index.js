7f1ddf2e8dbb7418871bc20808a9367952ec6843
icyaj289 <ajsung16@gmail.com>
2017-08-03 11:38:10 +0800

    Small error in Tokens(3)

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
+	if (req.query['req.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['req.challenge'])
 	}
 	res.send('Error, wrong token')
 })
