25b67075a234bdf29f5ae751a35a2b5b66b381c7
icyaj289 <ajsung16@gmail.com>
2017-08-11 22:53:28 +0800

    Add Check if Open (2)

--- index.js
+++ index.js
@@ -167,6 +167,13 @@ const actions = {
     fbRichMessage(recipientId, text);
   },
 
+  // Check if open
+  CheckIfOpen() {
+    var date = new Date();
+    var current_hour = date.getHours(); 
+    console.log(date, current_hour);
+  },
+    
   // Operating Hours
   OperatingHours(text) {
     console.log('Operating Hours Card')
@@ -174,13 +181,6 @@ const actions = {
     var text = '"message":{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"Art of Yoga - Operating Hours","image_url":"https://artofyoga.sg/wp-content/uploads/2015/11/16998817_1090435211063008_2620425424784904845_n.jpg","subtitle":"Monday - Friday: 6am - 9pm \\n Saturday & Sunday: 7:30am - 5pm","default_action": {"type": "web_url","url": "https://artofyoga.sg","messenger_extensions": true,"webview_height_ratio": "tall","fallback_url": "https://artofyoga.sg"}}]}}}';
     fbRichMessage(recipientId, text);
     CheckIfOpen();
-  },
-
-  // Check if open
-  CheckIfOpen() {
-    var date = new Date();
-    var current_hour = date.getHours(); 
-    console.log(date, current_hour);
   },
     
   // Ok
