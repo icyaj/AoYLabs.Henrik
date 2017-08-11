a906d4d3f4d92c9739c2985d91f0674719e1562a
icyaj289 <ajsung16@gmail.com>
2017-08-12 00:13:43 +0800

    Added isOpen (9)

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ const actions = {
     console.log(open);
       
     var recipientId = 1337595769686359;
-    var text = `"message":{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"Art of Yoga - Operating Hours","image_url":"https://artofyoga.sg/wp-content/uploads/2015/11/16998817_1090435211063008_2620425424784904845_n.jpg","subtitle":"Monday - Friday: 6am - 9pm \\n Saturday & Sunday: 7:30am - 5pm \\n #{open}","default_action": {"type": "web_url","url": "https://artofyoga.sg","messenger_extensions": true,"webview_height_ratio": "tall","fallback_url": "https://artofyoga.sg"}}]}}}`;
+    var text = '"message":{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"Art of Yoga - Operating Hours","image_url":"https://artofyoga.sg/wp-content/uploads/2015/11/16998817_1090435211063008_2620425424784904845_n.jpg","subtitle":"Monday - Friday: 6am - 9pm \\n Saturday & Sunday: 7:30am - 5pm \\n', open ,'","default_action": {"type": "web_url","url": "https://artofyoga.sg","messenger_extensions": true,"webview_height_ratio": "tall","fallback_url": "https://artofyoga.sg"}}]}}}';
     fbRichMessage(recipientId, text);
 
   },
