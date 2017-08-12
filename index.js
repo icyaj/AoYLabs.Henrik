18f929d35f0a546da883f88c72220c7507631453
icyaj289 <ajsung16@gmail.com>
2017-08-13 02:12:14 +0800

    Generic Msgs (21)

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ const actions = {
   Welcome(text) {
     var recipientId = sessions[text.sessionId];
     var res = recipientId.substr(1, 4);
-    console.log('Sessions: ', sessions[res]);
+    console.log('Sessions: ', res);
     console.log('Welcome Card');
     var recipientId = 1337595769686359;
     var text = '"message":{"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"Art of Yoga - A Boutique Yoga Studio in Singapore\'s East Coast ","image_url":"https://artofyoga.sg/wp-content/uploads/2017/02/AoY-Fullsize-0294.jpg","subtitle":"We offer a variety of yoga classes including Ashtanga, Hatha, Pre-Natal and more","default_action": {"type": "web_url","url": "https://www.instagram.com/p/rYlPzKtOqJ/","messenger_extensions": true,"webview_height_ratio": "tall","fallback_url": "https://www.instagram.com/artofyogasg/"},"buttons":[{"type":"web_url","url":"https://artofyoga.sg","title":"AoY Website"}]}]}}}';
