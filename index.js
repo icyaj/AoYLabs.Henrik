937b80d7f0aa0aa28e01eb4e7a5728dbab8bf7e3
icyaj289 <ajsung16@gmail.com>
2017-08-12 23:55:03 +0800

    Editing Operating Hours (5)

--- index.js
+++ index.js
@@ -179,17 +179,17 @@ const actions = {
     // Sets Day, Hour, Minute & open 
     var day = moment().tz("Asia/Singapore").weekday();  
     // var Hour = moment().tz("Asia/Singapore").hour();
-    var Hour = 9;
-    var Minute = moment().tz("Asia/Singapore").minute();
+    var hour = 9;
+    var minute = moment().tz("Asia/Singapore").minute();
     var isOpen = false;
     
     // Checks if Open
-    console.log(Hours,day);
+    console.log(hour,day);
     switch (day) {
     case(day >= 0 && day <= 5):
-        if (Hour >= 6 && Hour <= 21) { var isOpen = true; }
+        if (hour >= 6 && hour <= 21) { var isOpen = true; }
     case(day >= 6 && day <= 7) :
-        if ((Hour >= 8 && Hour <= 17) || (Hour == 7 && Minute >= 30)) { var isOpen = true; console.log('True'); }
+        if ((hour >= 8 && hour <= 17) || (hour == 7 && minute >= 30)) { var isOpen = true; console.log('True'); }
     }; 
     if (isOpen == true) {var open = 'Currently Open';} else {var open = 'Currently Closed'};
     console.log(isOpen, ' // ',open);
