bea5e519d297591765de6c9786caf4bec6bbd1cf
icyaj289 <ajsung16@gmail.com>
2017-09-01 16:23:14 +0800

    Seperate Class Teacher (41)

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ const GetTeachers = () => {
    // Loads Teacher File Names.
    var dir = 'Responses/Teacher/Teachers';
    var Teacher = [];
-   fs.readdir(dir, (err, files) => {return files.forEach(file => {Teacher.push(file);});})
+   fs.readdir(dir, (err, files) => {files.forEach(file => {GetTeachers.Teacher.push(file);});})
    console.log(Teacher); 
    // Init Variables
    var i = 0;
